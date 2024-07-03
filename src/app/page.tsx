1d1bc63b61cc9484ce613f24470a58fdaa647195
Kaung Htet Nyunt <162864681+kaunghn@users.noreply.github.com>
2024-07-03 21:02:52 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import Navbar from "@/components/Navbar";
+import Placeholder from "../components/Placeholder";
+import Carousel from "../components/Carousel";
+import ProfileList from "../components/ProfilesList";
 
 export default function Home(){
     return (
         <div>
             <Navbar></Navbar>
             <div>HOMEPAGE</div>
+            {/* <Carousel>
+                <Placeholder children="Person 1" desc="I am Ryan"></Placeholder>
+                <Placeholder children="Person 2" desc="I am Yan Hui"></Placeholder>
+                <Placeholder children="Person 3" desc="I am Kaung"></Placeholder>
+                <Placeholder children="Person 4" desc="I am Hnin"></Placeholder>
+                <Placeholder children="Person 5" desc="I am Nee"></Placeholder>
+            </Carousel> */}
+            <Placeholder name="Ryan" education="Singapore Management University" year="Sophomore" desc="Python Programming, OOP, React"/>
+            
         </div>
     )
 }
